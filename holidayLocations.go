6c5b026816e2a934f9cf5729fcc36259290648fc
Florian Uekermann <florian@uekermann.me>
2017-03-31 14:37:24 +0200

    Add swedish all saints day

--- holidayLocations.go
+++ holidayLocations.go
@@ -1,5 +1,7 @@
 package date
 
+import "time"
+
 // HolidaysByLocation is a map of holidays indexed by the location string returned by time.Location.String().
 // This map is used by the Date.IsHoliday() method.
 var HolidaysByLocation = map[string]Holidays{
@@ -7,6 +9,17 @@ var HolidaysByLocation = map[string]Holidays{
 		NewYearsEve, NewYearsDay, Epiphany, InternationalWorkersDay, ChristmasEve, ChristmasDay, SecondChristmasDay,
 		{Month: 6, Day: 6, Name: "National day of Sweden"},
 	}, Changing: []ChangingHoliday{
-		EasterAndFriends, MidsummerFriday,
+		EasterAndFriends, MidsummerFriday, allSaintsDaySweden,
 	}},
+}
+
+// allSaintsDaySweden matches the Saturday betweem October 31st and November 6th.
+var allSaintsDaySweden ChangingHoliday = func(year, month, day int) (string, bool) {
+	dayBefore := NewDate(year, int(time.October), 30, nil)
+	dayAfter := NewDate(year, int(time.November), 7, nil)
+	d := NewDate(year, month, day, nil)
+	if d.After(dayBefore) && d.Before(dayAfter) && d.IsWeekday(time.Saturday) {
+		return "All Saints' Day", true
+	}
+	return "", false
 }
